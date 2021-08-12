abe0386b955ae72087ef644ccdf76d3ac95be8b3
michedomingo <michedomingo@gmail.com>
2021-08-12 16:39:53 -0700

    Create React components

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+/**
+ * NOTES
+ * - a component is a function that returns UI
+ */
+function Header() {
+  return (
+    <header>
+      <h1>Michelle's Kitchen</h1>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <section>
+      <p>We serve the most delicious food around!</p>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>It's true.</p>
+    </footer>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <ul>
-    <li>Monday</li>
-    <li>Tuesday</li>
-    <li>Wednesday</li>
-  </ul>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
