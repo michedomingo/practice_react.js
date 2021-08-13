1aaab7d06ccb8be7c8d9abe65d0ef81862e51caf
michedomingo <michedomingo@gmail.com>
2021-08-12 20:21:17 -0700

    Conditional rendering (04_01)

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import restaurant from './restaurant.jpg';
 /**
  * NOTES
  * - a component is a function that returns UI
@@ -7,60 +6,20 @@ import restaurant from './restaurant.jpg';
  *      - when we render the component we pass the properties into the component
  *      - use dot notation in component to display information
  * - keys can help keep data in sync / render properly as app state changes over time
+ *
+ * - conditional rendering
  */
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
+
+function SecretComponent() {
+  return <h1>Secret info for authorized user only.</h1>;
 }
 
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food around!</p>
-      <img
-        src={restaurant}
-        height={200}
-        alt='silverware with napkin on top of plate at a restaurant table'
-      />
-      <ul style={{ textAlign: 'left' }}>
-        {props.dishes.map((dish) => (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
+function RegularComponent() {
+  return <h1>Everyone can see this component.</h1>;
 }
 
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
-
-const dishes = [
-  'Macaroni and Cheese',
-  'Salmon',
-  'Tofu with Vegetables',
-  'Minestrone',
-];
-
-// transormation function - turn list of items/dishes into objects
-const dishObjects = dishes.map((dish, i) => ({ title: dish, id: i }));
-// console.log(dishObjects);
-
-function App() {
-  return (
-    <div className='App'>
-      <Header name='Michelle' />
-      <Main adjective='amazing' dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+function App(props) {
+  return <>{props.authorized ? <SecretComponent /> : <RegularComponent />}</>;
 }
 
 export default App;
