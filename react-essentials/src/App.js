ef92ed237bf10556783cb70e9b53e7c9d952350a
michedomingo <michedomingo@gmail.com>
2021-08-12 17:05:54 -0700

    Add list to component

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -7,7 +7,6 @@ import './App.css';
  *      - use dot notation in component to display information
  */
 function Header(props) {
-  console.log(props);
   return (
     <header>
       <h1>{props.name}'s Kitchen</h1>
@@ -19,6 +18,11 @@ function Main(props) {
   return (
     <section>
       <p>We serve the most {props.adjective} food around!</p>
+      <ul style={{ textAlign: 'left' }}>
+        {props.dishes.map((dish) => (
+          <li>{dish}</li>
+        ))}
+      </ul>
     </section>
   );
 }
@@ -31,11 +35,13 @@ function Footer(props) {
   );
 }
 
+const dishes = ['Macaroni and Cheese', 'Salmon', 'Tofu with Vegetables'];
+
 function App() {
   return (
     <div className='App'>
       <Header name='Michelle' />
-      <Main adjective='amazing' />
+      <Main adjective='amazing' dishes={dishes} />
       <Footer year={new Date().getFullYear()} />
     </div>
   );
