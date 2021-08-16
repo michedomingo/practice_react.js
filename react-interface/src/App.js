cb5a684aea91e09f100213893df662848a82e8b7
michedomingo <michedomingo@gmail.com>
2021-08-16 13:25:39 -0700

    Properly fetch data from a fetch command and monitor for changes using useEffect and useCallback hooks (04_01)

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -1,8 +1,8 @@
+import { useState, useEffect, useCallback } from 'react';
 import { BiCalendar } from 'react-icons/bi';
 import Search from './components/Search';
 import AddAppointment from './components/AddAppointment';
 import AppointmentInfo from './components/AppointmentInfo';
-import appointmentList from './data.json';
 
 /**
  *
@@ -31,9 +31,63 @@ import appointmentList from './data.json';
  * use a key when looping through a series of elements
  * - destructuring: pass along info through appointment variable, received through AppointmentInfo component
  *      - pass different variables/objects/events, makes it easier to breakdown compnents into pieces
+ *
+ * data.json
+ * - normally data wouldn't be coming from your own project (in src folder)
+ *      - usually get data from an API, like GraphQL or your own REST API
+ *      - put data.json file in public folder to export with our project and be available as a URL (to simulate getting data from an API)
+ * - need to use other hooks for this to work in our React app
+ *      - useState, useEffect, useCallback
+ * - anything in the public folder will appear at the same level as our app, once it has been pushed up to a server
+ *      - in preview, it's automatically taking care of that for you
+ * - to React or to JavaScript and the browser, data.json will be in the same directory as everything else
+ *
+ * useEffect
+ * - lets you perform 'side-effects' in function components
+ *      - if you're familiar with how React lify cycle methods used to work
+ *          - think of useEffect as the component did mount, component did update, and component will unmount
+ * - perfect for data searching operations and manually changing the DOM when certain components are a result of these operations
+ * - this is how we're going to import our content from the state of the json file
+ *
+ * useState
+ * - start with useState variables for our appointment list with empty array as initial value
+ *
+ * JavaScript fetch API
+ * - allows us to retrieve an element either from a local file or from a server
+ * - take care of getting the data, then issue a promise
+ *      - our data will be retrieved and we will receive a response from the server we are asking the data from
+ *          - in this case, it will be our file directory
+ *      - use arrow function to take response from server then convert it to JSON
+ *          - bc when we normally receive data, it just comes back as text
+ *      - then use another promise, create temp variable called data where we receive all the data
+ *          - use setAppointmentList and pass through the process cata we received from the server
+ * - optionally, can pass throught variables to track (but not used in this project)
+ * - we're retrieving the data and asking useCallback to monitor any changes that happen to that data
+ *
+ * useEffect
+ * - make sure that it is tracking the data and any changes to the data as it comes in
+ * - side effect that your app doesn't normally worry about
+ *      - similar to manually controlling React a little bit more
+ * - issue the fetchData method then ask useEffect to track the fetching of data
+ *      - if the data changes for some reason, useEffect will keep track of it and update our app automatically
+ *
  */
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setAppointmentList(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className='App container mx-auto mt-3 font-thin'>
       <h1 className='text-5xl mb-3'>
