add3d87dc40f9471909bb16ee48b150dfc9d1eba
michedomingo <michedomingo@gmail.com>
2021-08-15 22:38:23 -0700

    Create component to add appointments (02_03)

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -1,5 +1,6 @@
-import { BiArchive } from 'react-icons/bi';
+import { BiCalendar } from 'react-icons/bi';
 import Search from './components/Search';
+import AddAppointment from './components/AddAppointment';
 
 /**
  *
@@ -16,16 +17,17 @@ import Search from './components/Search';
  * text-red-400: specify what you want to change to for color name/strength
  *         - anything above 500 is a shade, anything below 500 is a tint
  *
- * <BiArchive /> - call icon component as a HTML tag with properties
+ * <BiCalendar /> - call icon component as a HTML tag with properties
  */
 
 function App() {
   return (
     <div className='App container mx-auto mt-3 font-thin'>
-      <h1 className='text-5xl'>
-        <BiArchive className='inline-block text-red-400 align-top' />
+      <h1 className='text-5xl mb-3'>
+        <BiCalendar className='inline-block text-red-400 align-top' />
         Your Appointments
       </h1>
+      <AddAppointment />
       <Search />
     </div>
   );
