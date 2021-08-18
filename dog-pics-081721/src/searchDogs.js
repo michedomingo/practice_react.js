0deee193e7ae8e1e67c71fe2ce922ced462f2601
michedomingo <michedomingo@gmail.com>
2021-08-17 21:43:30 -0700

    Store user input in the 'input' field inside the 'query' state

--- dog-pics-081721/src/searchDogs.js
+++ dog-pics-081721/src/searchDogs.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SearchDogs() {
+  const [query, setQuery] = useState('');
+  console.log(query);
+
   return (
     <>
       <form className='form'>
@@ -12,6 +15,8 @@ export default function SearchDogs() {
           name='query'
           className='input'
           placeholder={`Try "corgie" or "beagle"`}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button type='submit' className='button'>
           Search
