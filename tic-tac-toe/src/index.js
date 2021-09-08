21aa5c51bd2c7a32f0eec7ae8d3b7a2725e03919
michedomingo <michedomingo@gmail.com>
2021-09-08 16:16:34 -0700

    Move handleClick method from Board to Game component. Concatenate new history entries into history within Game's handleClick method.

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -19,18 +19,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square
@@ -74,6 +62,25 @@ class Game extends React.Component {
       ],
       xIsNext: true,
     };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([
+        /* concat() method doesn’t mutate the original array */
+        {
+          squares: squares,
+        },
+      ]),
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
