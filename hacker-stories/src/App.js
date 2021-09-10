f1c0d677fdb2067ac299e0dfb4355930541b3970
michedomingo <michedomingo@gmail.com>
2021-09-10 15:45:26 -0700

    Experiment with displaying more of the item's properties

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -29,7 +29,16 @@ function App() {
 
       <ul>
         {list.map(function (item) {
-          return <li key={item.objectID}>{item.title}</li>;
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
         })}
       </ul>
     </div>
