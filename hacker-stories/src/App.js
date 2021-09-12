60a2c3a6ce9b331f7085485833c76fd205a94d4c
michedomingo <michedomingo@gmail.com>
2021-09-11 22:11:59 -0700

    Pass an inflexible key in the dependency array of custom and useEffect hooks

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const useSemiPersistentState = (initialState) => {
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem('search') || initialState
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
   );
 
   React.useEffect(() => {
-    localStorage.setItem('search', searchTerm);
-  }, [searchTerm]);
+    localStorage.setItem(key, value);
+  }, [value, key]);
 
-  return [searchTerm, setSearchTerm];
+  return [value, setValue];
 };
 
 const App = () => {
@@ -32,7 +32,7 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = useSemiPersistentState('React');
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
