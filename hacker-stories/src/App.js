f9dc4c51076dea7e105dce1a89ad39319222d4d0
michedomingo <michedomingo@gmail.com>
2021-09-11 20:51:15 -0700

    Implement useEffect Hook to trigger the side-effect each time the searchTerm changes

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -24,6 +24,10 @@ const App = () => {
     localStorage.getItem('search') || 'React'
   );
 
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
+
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
