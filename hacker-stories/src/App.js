f42c385fd7334503aebd5324497c3f2b90abe58b
michedomingo <michedomingo@gmail.com>
2021-09-12 19:36:37 -0700

    Use empty array of initial state, simulate fetching stories asynchronously

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -37,7 +37,13 @@ const getAsyncStories = () =>
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
-  const [stories, setStories] = React.useState(initialStories);
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then((result) => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const handleRemoveStory = (item) => {
     const newStories = stories.filter(
