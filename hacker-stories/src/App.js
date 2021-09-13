d7312fc330742fa3a02978b74d25cb8b4a874b9d
michedomingo <michedomingo@gmail.com>
2021-09-13 11:06:57 -0700

    Introduce state for error handling

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -41,14 +41,17 @@ const App = () => {
 
   const [stories, setStories] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
     setIsLoading(true);
 
-    getAsyncStories().then((result) => {
-      setStories(result.data.stories);
-      setIsLoading(false);
-    });
+    getAsyncStories()
+      .then((result) => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) => {
