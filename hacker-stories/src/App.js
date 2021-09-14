ac50c9d74c3e6fa4268c0501262782c0b2068049
michedomingo <michedomingo@gmail.com>
2021-09-13 17:38:30 -0700

    Update reducer function to cover new case in state transition

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -39,6 +39,8 @@ const getAsyncStories = () =>
 const storiesReducer = (state, action) => {
   if (action.type === 'SET_STORIES') {
     return action.payload;
+  } else if (action.type === 'REMOVE_STORY') {
+    return state.filter((story) => action.payload.objectID !== story.objectID);
   } else {
     throw new Error();
   }
